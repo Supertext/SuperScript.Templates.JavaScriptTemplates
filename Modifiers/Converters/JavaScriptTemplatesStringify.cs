259a660f98bbf1a2d74400026a442f94368f022b
Andrew Jameson <andrew_jameson@hotmail.com>
2015-01-03 19:59:59 +0100

    Initial upload of code.
    This commit represents the first time that the project is added to
    GitHub.

---
+++ Modifiers/Converters/JavaScriptTemplatesStringify.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SuperScript.Declarables;
+using SuperScript.Modifiers;
+using SuperScript.Templates.Declarables;
+using SuperScript.Templates.Modifiers.Converters;
+
+namespace SuperScript.Templates.JavaScriptTemplates.Modifiers.Converters
+{
+	/// <summary>
+	/// Converts the 'Declarations' property of the specified <see cref="PreModifierArgs"/> into an instance of <see cref="PostModifierArgs"/> 
+	/// where the 'Emitted' property contains the stringified JavaScript.
+	/// </summary>
+	public class JavaScriptTemplatesStringify : TemplateStringify
+	{
+		/// <summary>
+		/// <para>Enumerates each <see cref="TemplateDeclaration"/> and converts the declarations into a string.</para>
+		/// <para>If <see cref="TemplateStringify.PreCompile"/> is <c>true</c> then pre-compilation should occur in this method.</para>
+		/// </summary>
+		/// <param name="declarations">
+		/// A generic collection of <see cref="DeclarationBase"/> implementations. Only implementations which are of type 
+		/// <see cref="TemplateDeclaration"/> will be considered.
+		/// </param>
+		protected override string CollateTemplates(IEnumerable<DeclarationBase> declarations)
+		{
+			var templates = new StringBuilder();
+
+			if (PreCompile && !String.IsNullOrWhiteSpace(ScriptContents))
+			{
+				var engine = new Jurassic.ScriptEngine();
+				engine.Execute(ScriptContents);
+
+				foreach (TemplateDeclaration declaration in declarations.Where(d => d.GetType() == typeof(TemplateDeclaration)))
+				{
+					var precompiledTemplate = engine.CallGlobalFunction<string>("tmpl", declaration.Template);
+
+					templates.AppendFormat("var {0} = {1};\n", declaration.Name, precompiledTemplate);
+				}
+			}
+			else
+			{
+				foreach (TemplateDeclaration declaration in declarations.Where(d => d.GetType() == typeof (TemplateDeclaration)))
+				{
+					templates.AppendLine(declaration.ToString());
+				}
+			}
+
+			return templates.ToString();
+		}
+
+
+		/// <summary>
+		/// Default constructor for <see cref="JavaScriptTemplatesStringify"/> which has default locations in which to check for the
+		/// existence of the script file.
+		/// </summary>
+		public JavaScriptTemplatesStringify()
+		{
+			ScriptPath = "~/scripts/tmpl.min.js";
+
+			if (String.IsNullOrWhiteSpace(ScriptContents))
+			{
+				ScriptPath = "~/scripts/tmpl.js";
+			}
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SuperScript.Templates.JavaScriptTemplates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SuperScript.Templates.JavaScriptTemplates")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3b679f41-1bcf-4617-b0f9-d156e9558790")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
